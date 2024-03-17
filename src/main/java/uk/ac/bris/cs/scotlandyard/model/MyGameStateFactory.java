8b4c3ea80602e846247be28f4cb720724eea4155
MykolaTakun <takun1812@gmail.com>
2024-03-17 18:48:20 +0000

    first session

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,10 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
-
+import com.google.common.collect.ImmutableSet;
+import java.util.*;
 import javax.annotation.Nonnull;
-
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
 /**
@@ -13,12 +16,45 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
+	private final class MyGameState implements GameState {
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+		private MyGameState(
+				final GameSetup setup,
+				final ImmutableSet<Piece> remaining,
+				final ImmutableList<LogEntry> log,
+				final Player mrX,
+				final List<Player> detectives){
+			this.setup = setup;
+			this.remaining = remaining;
+			this.log = log;
+			this.mrX = mrX;
+			this.detectives = detectives;
+			if(setup.moves.isEmpty()) throw new IllegalArgumentException("Moves is empty!");
+		}
+		@Override public GameSetup getSetup() {  return setup; }
+		@Override  public ImmutableSet<Piece> getPlayers() { return remaining; }
+		@Override public Optional<Integer> getDetectiveLocation(Detective detective){
+			if(detective.isDetective()){
+
+			}
+			else return Optional.empty();
+		};
+
+		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece){ return };
+		@Override public GameState advance(Move move) {  return null;  }
+	}
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
+			return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
+		//throw new RuntimeException("Implement me!");
 
 	}
 
